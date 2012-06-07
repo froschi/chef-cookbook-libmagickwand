fd9061bed70998e3d98f262317c07153e456ba8a
Thorsten Fischer <thorsten@froschi.org>
2012-06-07 17:25:22 +0200

    Add dependencies for libmagickwand2.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,6 +1,23 @@
-packages = %w/
-  libmagickwand2
-/
+packages = Array.new
+
+case node[:lsb][:codename]
+when "lucid"
+  include_recipe "libbz2"
+  include_recipe "libfreetype"
+  include_recipe "libgomp"
+  include_recipe "libice"
+  include_recipe "libjpeg"
+  include_recipe "liblcms"
+  include_recipe "libltdl"
+  include_recipe "libmagickcore"
+  include_recipe "libpng"
+  include_recipe "libsm"
+  include_recipe "libtiff"
+  include_recipe "libxext"
+  include_recipe "libxt"
+
+  packages |= %w/libmagickwand2/
+end
 
 packages.each do |pkg|
   package pkg do
